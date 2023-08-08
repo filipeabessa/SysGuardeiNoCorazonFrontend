684381fccd1f24d9dfa1d260205cf8fb1e937324
filipegbessa <filipegbessa@gmail.com>
2023-08-08 20:39:07 -0300

    feat(): add more info to DisaffectionCard component

--- src/components/dataDisplay/DisaffectionCard/index.tsx
+++ src/components/dataDisplay/DisaffectionCard/index.tsx
@@ -28,8 +28,27 @@ const DisaffectionCard: FC<DisaffectionCardProps> = ({
     >
       <Typography
         variant="body1"
+        color="secondary.main"
       >
         <strong>Id:</strong>&nbsp; {disaffection?.id}
+      </Typography>
+      <Typography
+        variant="body1"
+        color="secondary.main"
+      >
+        <strong>Descrição: </strong>&nbsp; {disaffection?.description}
+      </Typography>
+      <Typography
+        variant="body1"
+        color="secondary.main"
+      >
+        <strong>Testemunhas: </strong>&nbsp; {disaffection?.witnesses}
+      </Typography>
+      <Typography
+        variant="body1"
+        color="secondary.main"
+      >
+        <strong>Envolvidos: </strong>&nbsp; {disaffection?.involvedPeople}
       </Typography>
     </Card>
   );
